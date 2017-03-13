3ef1ff64ad7f365835e232edf9cb6a57fe91b675
Alexey <trueflywood@gmail.com>
2017-03-13 22:18:12 +0300

    finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app works!';
+  range: string = '10';
+    onChange(event, par): void {
+        console.log(event, par);
+    }
+    countPiople: number = 5;
+    minus(): void {
+        this.countPiople = (this.countPiople > 1) ? this.countPiople - 1 : 1;
+        console.log(this.countPiople);
+    }
+    plus(): void {
+        this.countPiople++ ;
+    }
+
 }
